75bfb9ce1bc04a979d41e1e3f1f7f014bbe0a448
Miguel <miguelsousalima@hotmail.com>
2020-08-09 10:37:35 -0300

    Ajutes pagine profile e inicio da Pages Repo

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={"username"} to={"/miguelslima"}>
+          miguelslima
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to={"miguelslima/NextLevelWeek-02"}>
+          NextLevelWeek-02
+        </Link>
+      </Breadcrumb>
+
+      <p>Projeto desenvolvido na Next Level Week 02</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/miguelslima/NextLevelWeek-02"}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
